6e5cb3da8d3432c16490079c0fadebf0e367e85b
Jonnas Costa <jonn567@gmail.com>
2021-12-12 15:27:08 +0000

    refactor main

--- main.cc
+++ main.cc
@@ -1,8 +1,6 @@
 #include <gtk/gtk.h>
 
-static void
-activate(GtkApplication *app,
-         gpointer user_data)
+static void activate(GtkApplication *app, gpointer user_data)
 {
   GtkWidget *window;
 
@@ -16,10 +14,8 @@ int main(int argc, char **argv)
   GtkApplication *app;
   int status;
 
-  app = gtk_application_new("org.gtk.example",
-                            G_APPLICATION_FLAGS_NONE);
-  g_signal_connect(app, "activate",
-                   G_CALLBACK(activate), NULL);
+  app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
   status = g_application_run(G_APPLICATION(app), argc, argv);
   g_object_unref(app);
   return (status);
